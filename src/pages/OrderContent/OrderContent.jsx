3b1c87fd356c5674e8e2c2da1c0011910dc2113b
cos4338@gmail.como <seungyeonchoo@chuseung-yeon-ui-MacBookAir.local>
2022-09-04 01:22:49 +0900

    Feat: order-content Dummy data 생성

--- src/pages/OrderContent/OrderContent.jsx
+++ src/pages/OrderContent/OrderContent.jsx
@@ -2,6 +2,30 @@ import DeliveryInfo from './components/DeliveryInfo';
 import OrderStatus from './components/OrderStatus';
 import PaymentInfo from './components/PaymentInfo';
 import styled from 'styled-components';
+const OrderContentDummy = [
+  {
+    orderNumber: 1,
+    orderProduct: {
+      product_name: '목감기에 좋은 건더기 없는 프리미엄 수제 착즙 유자청',
+      product_img: 'https://cdn.imweb.me/thumbnail/20220610/7902f9ea75514.jpg',
+      product_price: 15000,
+      product_discount: 13300,
+      delivery_fee: 3500,
+      delivery_method: '택배',
+    },
+    payment_method: true, // true = 카드결제, false = 무통장입금
+    orderer_info: {
+      orderer_name: '추승연',
+      orderer_contact: '010-1234-5678',
+    },
+    shipping_info: {
+      postcode: 1234,
+      order_address: '서울시 양천구 신정3동 신정로7길 60-7',
+      detail_address: '402 - 701',
+      shipping_req: '부재시 경비실에 맡겨주세요.',
+    },
+  },
+];
 
 const OrderContent = () => {
   return (
