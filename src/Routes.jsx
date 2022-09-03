ac10f11faed4a507bfa8122c16c00ecbce091ea3
jihyun-jeon <jhplus13@naver.com>
2022-09-03 22:04:06 +0900

    Feat:registerList 라우터 주소 추가

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Nav from './components/Nav';
 import Order from './pages/Order/Order';
+import RegisterList from './pages/RegisterList/RegisterList';
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
       <Nav />
       <Routes>
         <Route path="/" element={<Order />} />
+        <Route path="/register_list" element={<RegisterList />} />
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/pages/RegisterList/RegisterList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const RegisterList = () => {
+  return (
+    <Container>
+      <h1>RegisterList</h1>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  margin: 2rem;
+  background-color: aliceblue;
+`;
+
+export default RegisterList;
