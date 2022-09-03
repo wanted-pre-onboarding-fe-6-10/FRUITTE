55a06fe764a04e89697a2d7e363f48394ebd4261
cos4338@gmail.como <seungyeonchoo@chuseung-yeon-ui-MacBookAir.local>
2022-09-03 22:40:48 +0900

    Feat: ordercontent 라우터 주소 추가

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Nav from './components/Nav';
 import Order from './pages/Order/Order';
+import OrderContent from './pages/OrderContent/OrderContent';
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
       <Nav />
       <Routes>
         <Route path="/" element={<Order />} />
+        <Route path="/ordercontent" element={<OrderContent />} />
       </Routes>
       <Footer />
     </BrowserRouter>
