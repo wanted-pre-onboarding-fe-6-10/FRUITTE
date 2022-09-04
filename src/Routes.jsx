490c4303cda7d110703f05fa6ed96fb225a3e319
Dev-jwJeong <rgn95652@gmail.com>
2022-09-04 13:34:04 +0900

    Feat : 상품목록 페이지 url 설정(/fruitstore)

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Nav from './components/Nav';
-import Order from './pages/Order/Order';
+import ProductList from './pages/ProductList/ProductList';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Nav />
       <Routes>
-        <Route path="/" element={<Order />} />
+        <Route path="/fruitstore" element={<ProductList />} />
       </Routes>
       <Footer />
     </BrowserRouter>
