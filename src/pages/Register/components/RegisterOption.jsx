dd4c173f180c4fa7e42b9e5319e63968bd999ca9
윤관 <blackgar@naver.com>
2022-09-04 23:22:02 +0900

    feat: 옵션 추가됐을 때 아래에 옵션 추가한 내용 보이도록 구현

--- src/pages/Register/components/RegisterOption.jsx
+++ src/pages/Register/components/RegisterOption.jsx
@@ -1,38 +1,59 @@
 // 옵션 입력 컴포넌트, 옵션명, 가격, 재고수량 입력할 수 있는 컴포넌트
 
 import { useState } from 'react';
+import styled from 'styled-components';
 import RegisterBox from './common/RegisterBox';
+import RegisterOptionList from './RegisterOptionList';
 import {
   RegisterContentWrapper,
   RegisterInputWrapper,
   RegisterTitleWrapper,
 } from './RegisterPrice';
 
-const RegisterOption = () => {
-  const [optionList, setOptionList] = useState([]);
+const RegisterOption = ({ data, setData }) => {
+  const [option, setOption] = useState({ productName: '', price: 0, quantity: 0 });
+  const onSubmit = e => {
+    e.preventDefault();
+    const ops = [...data.options, option];
+    setData({ ...data, options: ops });
+  };
   return (
     <RegisterBox title={'옵션'}>
-      <RegisterContentWrapper>
-        <RegisterTitleWrapper>옵션명</RegisterTitleWrapper>
-        <RegisterInputWrapper>
-          <input type="text" />
-        </RegisterInputWrapper>
-      </RegisterContentWrapper>
-      <RegisterContentWrapper>
-        <RegisterTitleWrapper>가격</RegisterTitleWrapper>
-        <RegisterInputWrapper>
-          <input type="text" />
-        </RegisterInputWrapper>
-      </RegisterContentWrapper>
-      <RegisterContentWrapper>
-        <RegisterTitleWrapper>수량</RegisterTitleWrapper>
-        <RegisterInputWrapper>
-          <input type="text" />
-        </RegisterInputWrapper>
-      </RegisterContentWrapper>
-      <button>추가하기</button>
+      <OptionForm onSubmit={onSubmit}>
+        <RegisterContentWrapper>
+          <RegisterTitleWrapper>옵션명</RegisterTitleWrapper>
+          <RegisterInputWrapper>
+            <input
+              type="text"
+              onChange={e => setOption({ ...option, productName: e.target.value })}
+            />
+          </RegisterInputWrapper>
+        </RegisterContentWrapper>
+        <RegisterContentWrapper>
+          <RegisterTitleWrapper>가격</RegisterTitleWrapper>
+          <RegisterInputWrapper>
+            <input
+              type="text"
+              onChange={e => setOption({ ...option, price: parseInt(e.target.value) })}
+            />
+          </RegisterInputWrapper>
+        </RegisterContentWrapper>
+        <RegisterContentWrapper>
+          <RegisterTitleWrapper>수량</RegisterTitleWrapper>
+          <RegisterInputWrapper>
+            <input
+              type="text"
+              onChange={e => setOption({ ...option, quantity: parseInt(e.target.value) })}
+            />
+          </RegisterInputWrapper>
+        </RegisterContentWrapper>
+        <button style={{ width: '100%' }}>추가하기</button>
+      </OptionForm>
+
+      <RegisterOptionList options={data.options} />
     </RegisterBox>
   );
 };
+const OptionForm = styled.form``;
 
 export default RegisterOption;
