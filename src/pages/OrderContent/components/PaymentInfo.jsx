4ffe00f040e617389bdca816aed7a163589fe109
cos4338@gmail.como <seungyeonchoo@chuseung-yeon-ui-MacBookAir.local>
2022-09-04 15:26:23 +0900

    Feat: 결제 정보 컴포넌트 생성

--- src/pages/OrderContent/components/PaymentInfo.jsx
+++ src/pages/OrderContent/components/PaymentInfo.jsx
@@ -1,22 +1,40 @@
 import Info from './Info';
 import styled from 'styled-components';
+import BoxTitle from './BoxTitle';
 
-const PaymentInfo = () => {
+const PaymentInfo = ({ paymentInfo }) => {
+  const payMethod = paymentInfo.payment_method;
+  const payAccount = paymentInfo.account_info;
+  const accountInfos = [payAccount.bank_name, payAccount.account_number, payAccount.account_name];
+
   return (
     <PaymentBox>
-      <Info title="결제방식" />
-      <Info title="카드정보" />
-      <Info title="입금계좌" />
-      <Info title="결제금액" />
+      <BoxTitle title="결제정보" />
+      <Info title="결제방식" text={payMethod ? '카드결제' : '무통장입금'} />
+      {payMethod && <Info title="카드정보" text={paymentInfo.card_number} />}
+      {!payMethod && (
+        <Info
+          title="입금계좌"
+          text={accountInfos.map(item => (
+            <>
+              {item} <br />
+            </>
+          ))}
+        />
+      )}
+      <Info title="결제금액" text={paymentInfo.total_price} />
     </PaymentBox>
   );
 };
 
 const PaymentBox = styled.div`
-  width: 30%;
+  width: 50%;
   height: 100%;
   display: flex;
+  margin-top: 20px;
+  padding: 10px 30px;
   flex-direction: column;
+  border: ${props => `1px solid ${props.theme.borderColor}`};
 `;
 
 export default PaymentInfo;
