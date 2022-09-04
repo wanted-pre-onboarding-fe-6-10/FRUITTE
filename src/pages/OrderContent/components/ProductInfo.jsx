f7a1b6b7e5f464e4077388035487b843f81b226c
cos4338@gmail.como <seungyeonchoo@chuseung-yeon-ui-MacBookAir.local>
2022-09-04 15:23:58 +0900

    Feat: 주문 상품 컴포넌트 생성

--- src/pages/OrderContent/components/ProductInfo.jsx
+++ src/pages/OrderContent/components/ProductInfo.jsx
@@ -1,38 +1,60 @@
 import styled from 'styled-components';
+import Info from './Info';
 
-const ProductInfo = ({ orderInfo }) => {
+const ProductInfo = ({ productInfo, priceInfo }) => {
+  const withComma = price => price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  const itemPrice = productInfo.isDiscount ? priceInfo.discount_price : priceInfo.order_price;
+  const deliveryFee = priceInfo.delivery_fee;
+  const itemPriceWithComma = withComma(itemPrice);
+  const deliveryFeeWithComma = withComma(deliveryFee);
+  const totalPrice = withComma(itemPrice + deliveryFee);
+
   return (
     <ProductBox>
-      <ProductImage src={orderInfo.product_image} />
+      <ProductImage src={productInfo.product_img} />
       <Wrapper>
-        <Text>item-name{orderInfo.product_image}</Text>
-        <SubText>item-option</SubText>
-        <Text>price + delivery_fee = total price</Text>
+        <Info title="주문번호" />
+        <Text>{productInfo.product_name}</Text>
+        <SubText>{productInfo.order_option}</SubText>
+        <Text bold="bold">
+          {itemPriceWithComma} + {deliveryFeeWithComma} = {totalPrice}
+        </Text>
       </Wrapper>
     </ProductBox>
   );
 };
 
 const ProductBox = styled.div`
+  width: 50%;
+  padding: 20px;
   display: flex;
+  justify-content: center;
+  border: ${props => `1px solid ${props.theme.borderColor}`};
 `;
 
 const Wrapper = styled.div`
+  width: 75%;
+  margin: 0 20px;
   display: flex;
   flex-direction: column;
+  justify-content: center;
 `;
 
 const Text = styled.h1`
-  color: ${props => props.theme.ownColor};
+  color: ${props => props.theme.textColor};
+  font-weight: ${props => props.bold};
+  padding: 15px;
 `;
 
 const SubText = styled.h1`
   color: ${props => props.theme.subTextColor};
+  padding: 15px;
 `;
 
 const ProductImage = styled.img.attrs(props => ({
   src: props.src,
-  size: props.size || '1em',
-}))``;
+}))`
+  width: 25%;
+`;
 
 export default ProductInfo;
